a75de64a399dcbb782f68b01ee270beaa5c62cc5
Matheus Sampaio <matheus@sampaio.us>
2021-02-20 17:29:13 -0600

    chore: update website title and disable favicon for now

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   lintOnSave: process.env.NODE_ENV !== "production",
   publicPath:
-    process.env.NODE_ENV === "production" ? "/albion-loot-logger-helper" : "/"
+    process.env.NODE_ENV === "production" ? "/albion-loot-logger-helper" : "/",
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Loot Logger Helper - Albion Online'
+    }
+  }
 }
